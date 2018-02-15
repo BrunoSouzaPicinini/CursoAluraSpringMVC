e06cc8f04827b9b522b4662cdb78100095cd00a4
BrunoSouzaPicinini <brunosouzapicinini@gmail.com>
2018-02-14 23:23:44 -0200

    Cadastro de Produtos

--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,13 +1,24 @@
 package br.com.casadocodigo.loja.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produto {
 
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
 	private String titulo;
 	private String descricao;
 	private int paginas;
 	public String getTitulo() {
 		return titulo;
 	}
+	 
+	
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
@@ -23,6 +34,15 @@ public class Produto {
 	public void setPaginas(int paginas) {
 		this.paginas = paginas;
 	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	@Override
 	public String toString() {
 		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
