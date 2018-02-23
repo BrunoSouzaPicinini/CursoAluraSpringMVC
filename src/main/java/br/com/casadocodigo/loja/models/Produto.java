e4c758adf1aeb5445c2355a69e6ee5f4e715488e
BrunoSouzaPicinini <brunosouzapicinini@gmail.com>
2018-02-23 00:16:05 -0300

    Preco de Produto

--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,9 +1,13 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 @Entity
 public class Produto {
@@ -14,10 +18,12 @@ public class Produto {
 	private String titulo;
 	private String descricao;
 	private int paginas;
+	@ElementCollection
+	private List<Preco> precos;
+	
 	public String getTitulo() {
 		return titulo;
 	}
-	 
 	
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
@@ -41,6 +47,16 @@ public class Produto {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+	
+	
+
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
 	}
 
 	@Override
